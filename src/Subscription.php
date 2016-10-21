624a8bba89f97e4fc44df3f124610883ecb0757a
Dale Inverarity <dale@diit.ca>
2016-10-21 00:55:22 -0400

    Update Subscription.php

--- src/Subscription.php
+++ src/Subscription.php
@@ -5,7 +5,7 @@ namespace Laravel\Cashier;
 use Carbon\Carbon;
 use LogicException;
 use DateTimeInterface;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as Model;
 
 class Subscription extends Model
 {
